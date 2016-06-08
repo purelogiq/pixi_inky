15b225c4d958bd37ef81b95539cfbd7ceb1930a1
Israel Madueme <purelogiq@gmail.com>
2016-06-08 12:04:04 -0400

    Updated Config

--- js/Config.js
+++ js/Config.js
@@ -1,3 +1,15 @@
 var INKY = {
-    rootElement: document.getElementById('inky-root')
+    rootElement: document.getElementById('inky-root'),
+    WORLD_WIDTH: 1000,
+    WORLD_HEIGHT: 1000,
+    GENDERS: ['m', 'f'],
+    SIZES: ['S', 'M', 'L'],
+    NUM_EYES: 5,
+    NUM_SKIN_TONES: 6,
+    NUM_HAIR_STYLES_PER_GENDER: 3,
+    NUM_SHOES: 8,
+    HAIR_COLORS: ['black', 'brown', 'blonde', 'grey', 'red'],
+    PANT_COLORS: ['blue', 'brown', 'green', 'navy', 'tan'],
+    SHIRT_COLORS: ['blue', 'green', 'grey', 'navy', 'pine', 'red', 'white', 'yellow'],
+    SHIRT_DESIGNS: []
 };
--- js/Inky.js
+++
@@ -1,1 +0,0 @@
-var Inky = new Sprite();
